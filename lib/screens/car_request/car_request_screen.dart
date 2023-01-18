496463cb9d2eab07e63f80f7a1b9c90d264b6862
OeungChheanghok123 <chheanghok44@gmail.com>
2023-01-18 18:22:10 +0700

    18-Jan-2023

--- lib/screens/car_request/car_request_screen.dart
+++ lib/screens/car_request/car_request_screen.dart
@@ -1,5 +1,6 @@
 import 'package:driver_app/constants/constants.dart';
 import 'package:driver_app/screens/main_screen/main_screen_view_model.dart';
+import 'package:driver_app/utility/button_app_bar.dart';
 import 'package:driver_app/utility/text_style.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -19,7 +20,7 @@ class CarRequestScreen extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           appBar,
-          buttonAppBar(context),
+          const ButtonAppBar(),
         ],
       ),
     );
@@ -34,85 +35,4 @@ class CarRequestScreen extends StatelessWidget {
     );
   }
 
-  Widget buttonAppBar(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.symmetric(horizontal: defaultPaddin),
-      width: MediaQuery.of(context).size.width,
-      height: 50,
-      decoration: const BoxDecoration(
-        color: whiteColor,
-        border: Border(
-          top: BorderSide(width: 1, color: primaryGrayColor),
-          bottom: BorderSide(width: 1, color: primaryGrayColor),
-        ),
-      ),
-      child: Row(
-        children: [
-          TextButton(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Icon(
-                  Icons.feed,
-                  color: blackColor,
-                ),
-                const SizedBox(width: 3),
-                Text("Save", style: AppTextStyle.headline2),
-              ],
-            ),
-            onPressed: () {},
-          ),
-          const SizedBox(width: defaultPaddin),
-          TextButton(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Icon(
-                  Icons.edit_note,
-                  color: blackColor,
-                ),
-                const SizedBox(width: 3),
-                Text("Edit", style: AppTextStyle.headline2),
-              ],
-            ),
-            onPressed: () {},
-          ),
-          const SizedBox(width: defaultPaddin),
-          TextButton(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Icon(
-                  Icons.restore,
-                  color: blackColor,
-                ),
-                const SizedBox(width: 3),
-                Text("Cancel", style: AppTextStyle.headline2),
-              ],
-            ),
-            onPressed: () {},
-          ),
-          const SizedBox(width: defaultPaddin),
-          SizedBox(
-            width: 200,
-            height: 30,
-            child: TextField(
-              style: AppTextStyle.headline2,
-              decoration: InputDecoration(
-                border: const OutlineInputBorder(),
-                hintText: 'Record ID',
-                hintStyle:
-                    AppTextStyle.headline2.copyWith(color: secondGraydColor),
-                contentPadding: const EdgeInsets.only(
-                  left: 10,
-                  top: 0,
-                  bottom: 0,
-                ),
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
 }
